0e9aba3727a25fd001376894c982af0965710d9b
Grigory <grigory@clearscale.net>
2019-02-24 23:56:27 +0700

    print commands to a sync-session plan file

--- src/main/kotlin/tga/folder_sync/Init.kt
+++ src/main/kotlin/tga/folder_sync/Init.kt
@@ -7,12 +7,15 @@ import tga.folder_sync.files.FoldersFactory
 import tga.folder_sync.files.SFile
 import tga.folder_sync.tree.Tree
 import tga.folder_sync.tree.TreeSyncCommands
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * Created by grigory@clearscale.net on 2/21/2019.
  */
 private val logger: Logger = LoggerFactory.getLogger("tga.folder_sync.init")
-
+private val outDir: String = System.getProperty("outDir") + SimpleDateFormat("yyyy-MM-dd").format(Date())
 
 fun init(args: Array<String>) {
     if (args.size < 3) throw RuntimeException("not enough parameters!")
@@ -47,7 +50,7 @@ fun init(args: Array<String>) {
 
 }
 
-fun printTree(title: String, tree: Tree<SFile>) {
+private fun printTree(title: String, tree: Tree<SFile>) {
     println(title)
     tree.deepFirstTraversWithLevel(" ") { node, prefix->
         println("${prefix}${node.obj.name}")
@@ -56,38 +59,39 @@ fun printTree(title: String, tree: Tree<SFile>) {
 
 }
 
-fun printCommands(commands: TreeSyncCommands<SFile>, srcFolder: SFile, dstFolder: SFile) {
+private fun printCommands(commands: TreeSyncCommands<SFile>, srcFolder: SFile, dstFolder: SFile) {
 
     fun fileOrFolder(item: SFile) = when (item.isDirectory) {
         true  -> "<folder>"
         false -> "< file >"
     }
 
+    val folder = File(outDir)
 
+    if (!folder.exists()) {
+        folder.mkdir()
+    }
 
-    for (src in commands.toAdd) {
-        val srcFile = src.obj
-
-        if (srcFile.isDirectory) {
-            ////// ----- create a folder -----
+    File("$outDir/plan.txt").printWriter().use { out ->
+        for (src in commands.toAdd) {
+            val srcFile = src.obj
             val dstFileName = "${dstFolder.absolutePath}${dstFolder.pathSeparator}${srcFile.relativeTo(srcFolder) }"
 
-            println("  mk <folder> [$dstFileName]")
+            if (srcFile.isDirectory) { ////// ----- create a folder -----
+                out.println("  mk <folder> $dstFileName")
 
-        } else {
-            ////// ----- copy a file -----
-            val srcFileName = srcFile.absolutePath
-            val dstFileName = "${dstFolder.absolutePath}${dstFolder.pathSeparator}${srcFile.relativeTo(srcFolder) }"
+            } else { ////// ----- copy a file -----
+                val srcFileName = srcFile.absolutePath
+                out.println("copy < file > $srcFileName --> $dstFileName")
+            }
+        }
 
-            println("copy < file > [$srcFileName] [$dstFileName]")
+        for (dstNode in commands.toRemove) {
+            val dstFile = dstNode.obj
+            val dstFileName = "${dstFolder.absolutePath}${dstFolder.pathSeparator}${dstFile.relativeTo(dstFolder) }"
 
+            out.println(" del ${fileOrFolder(dstFile)} $dstFileName")
         }
     }
 
-    for (dstNode in commands.toRemove) {
-        val dstFile = dstNode.obj
-        val dstFileName = "${dstFolder.absolutePath}${dstFolder.pathSeparator}${dstFile.relativeTo(dstFolder) }"
-
-        println(" del ${fileOrFolder(dstFile)} [$dstFileName]")
-    }
 }
