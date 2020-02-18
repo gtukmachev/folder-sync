3b652ea8c46b669010856f5eae0ef5c61202739f
Grigory <grigory@clearscale.net>
2020-02-18 15:09:16 +0700

    css stylefor ui

--- src/main/kotlin/tga/folder_sync/fx/MainFx.kt
+++ src/main/kotlin/tga/folder_sync/fx/MainFx.kt
@@ -13,8 +13,6 @@ import javafx.scene.control.PasswordField
 import javafx.scene.control.TextField
 import javafx.scene.layout.GridPane
 import javafx.scene.paint.Color
-import javafx.scene.text.Font
-import javafx.scene.text.FontWeight
 import javafx.scene.text.Text
 import javafx.stage.Stage
 
@@ -34,27 +32,28 @@ class MainFx : Application() {
             padding = Insets(25.0)
         }
 
-        Text("Welcome").apply {
-            font = Font.font("Tahoma", FontWeight.NORMAL, 20.0)
-        }.also { rootGrid.add( it, 0,0, 2,1 ) }
+        Text("Welcome").apply { id = "welcome-text" }.also { rootGrid.add( it, 0,0, 2,1 ) }
 
         Label("User Name:").also { rootGrid.add( it, 0, 1) }
         TextField()        .also { rootGrid.add( it, 1, 1) }
         Label("Password:") .also { rootGrid.add( it, 0, 2) }
         PasswordField()    .also { rootGrid.add( it, 1, 2) }
 
-        val actiontarget = Text().also { rootGrid.add( it, 1, 6) }
+        val actionTarget = Text().apply { id = "action-target"; rootGrid.add( this, 1, 6) }
 
         Button("Sign in").apply { rootGrid.add( this, 1, 4) }
             .onAction = EventHandler<ActionEvent> {
-                actiontarget.fill = Color.FIREBRICK
-                actiontarget.text = "Button pressed"
+                actionTarget.fill = Color.FIREBRICK
+                actionTarget.text = "Button pressed"
             }
 
         primaryStage.apply {
             title = "Sign In"
             scene = Scene(rootGrid, 300.0, 250.0)
-            scene.stylesheets.add( this.javaClass.getResource("Login.css").toExternalForm() )
+            val clz = MainFx::class.java
+            print(clz.name)
+            val css = clz.getResource("Login.css")
+            scene.stylesheets.add( css.toExternalForm() )
         }.show()
 
     }
